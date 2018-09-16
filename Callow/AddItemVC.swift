31fe1dda1287495ec40b394a3f8d8a6eedf43ee9
Thomas Smith <thsm@umich.edu>
2018-09-16 16:19:22 -0400

    Add red border to empty (invalid) title text fields when adding new items

--- Callow/AddItemVC.swift
+++ Callow/AddItemVC.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Thomas Smith. All rights reserved.
 //
 
+import CoreGraphics
 import UIKit
 
 class AddItemVC: UIViewController {
@@ -14,6 +15,11 @@ class AddItemVC: UIViewController {
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var dateEntry: UIDatePicker!
     @IBOutlet weak var descriptionView: UITextView!
+    
+    private let incompleteBorderColor: UIColor = .red
+    private let incompleteBorderWidth: CGFloat = 1.0
+    private let incompleteCornerRadius: CGFloat = 5.0
+    private var isTitleFieldIncomplete = false
     
     // MARK: - Overrides
     override func viewDidLoad() {
@@ -24,7 +30,14 @@ class AddItemVC: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-    // MARK: - Navigation
+    // MARK: - Navigation and Other IBActions
+    @IBAction func titleFieldEditingChanged(_ sender: UITextField) {
+        // Remove the incomplete border around the text field if the user has now entered text
+        if isTitleFieldIncomplete && titleField.text != nil && titleField.text != "" {
+            isTitleFieldIncomplete = false
+            titleField.layer.borderWidth = 0.0
+        }
+    }
     @IBAction func cancelNewItem(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
     }
@@ -36,7 +49,10 @@ class AddItemVC: UIViewController {
             let desc = descriptionView.text ?? ""
             
             if title == "" {
-                titleField.tintColor = UIColor.red
+                titleField.layer.borderColor = incompleteBorderColor.cgColor
+                titleField.layer.borderWidth = incompleteBorderWidth
+                titleField.layer.cornerRadius = incompleteCornerRadius
+                isTitleFieldIncomplete = true
                 return // Return prematurely so that the AddItemVC is not dismissed
             }
             else if !delegate.dataManager.addItem(title: title, date: date, desc: desc) {
